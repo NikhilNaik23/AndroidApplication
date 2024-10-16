d91131a65ba47c81c677d23c737dc80f0784d375
Nikhil <nenavathnikhil2@gmail.com>
2024-10-16 11:17:10 +0530

    second commit

---
+++ app/src/main/java/com/example/project2/MenuBar.kt
@@ -0,0 +1,48 @@
+package com.example.project2
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu_bar)
+        val toolBar=findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolBar)
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.main_menu,menu)
+            return true
+        }
+        override fun onOptionsItemSelected(item: MenuItem): Boolean{
+            return when(item.itemId)
+            {
+                R.id.i1->{
+                    Toast.makeText(this,"One is Clicked!",Toast.LENGTH_LONG).show()
+                    true
+                }
+                R.id.i2->{
+                    Toast.makeText(this,"Two is Clicked!",Toast.LENGTH_LONG).show()
+                    true
+                }
+                R.id.i3->{
+                    Toast.makeText(this,"Three is Clicked!",Toast.LENGTH_LONG).show()
+                    true
+                }
+                R.id.i4->{
+                    Toast.makeText(this,"Four is Clicked!",Toast.LENGTH_LONG).show()
+                    true
+                }
+
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
+    }
+}
